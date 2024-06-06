e042b74ae9734b429ca0120563c71312b3a93932
Nimona123 <134584118+Nimona123@users.noreply.github.com>
2024-06-06 16:46:44 +0300

    Delete 1-concat.py

--- 1-concat.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""concatenate two strings"""
-
-
-def concat(str1: str, str2: str) -> str:
-    """ function concat that takes a string str1 and a string str2
-    and returns a concatenated string"""
-    return str1 + str2
